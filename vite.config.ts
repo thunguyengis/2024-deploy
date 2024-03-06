753324b840feaa01bbc00b2e02b4649fc68f5631
thunguyengis <thunguyengis@gmail.com>
2024-03-06 17:38:09 +0700

    hhh

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base:"https://thunguyengis.github.io/2024-deploy/",
+  base:"/2024-deploy",
 })
